c39dd2cc34e50d098f39a74b5b473920473bb378
Raina Imtiyaz <147962517+rainaimtiyaz@users.noreply.github.com>
2025-05-23 17:03:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ with col_data:
 st.markdown("""
 <div style="display: inline-flex; align-items: center; gap: 4px; margin-bottom: 0rem;">
     <span style="font-size: 1.7rem; font-weight: 600.1;">🔍 Masukkan Preferensi Anda</span>
-    <div class="tooltip">asdf
+    <div class="tooltip">ℹ
         <span class="tooltiptext">
             Rekomendasi dihasilkan berdasarkan perhitungan Jaccard Similarity<br>
             <br>
